80162d7316d1c5b4914b680d311996a07073f668
ferdinand <ferdinand.sijtsma@student.hu.nl>
2020-04-15 15:42:25 +0200

    Class genaamt boek met title, Author en category toegevoegd

---
+++ boek.java
@@ -0,0 +1,34 @@
+public class boek {
+
+	private int Title;
+	private int Author;
+	private int Category;
+
+	public void getTitle()
+		throw new UnsupportedOperationException();
+	}
+
+	public void setTitle(int Title) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void getAuthor() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void setAuthor(int Author) {
+
+		throw new UnsupportedOperationException();
+	}
+
+	public void getCategory() {
+
+		throw new UnsupportedOperationException();
+	}
+
+	public void setCategory(int Category) {
+
+		throw new UnsupportedOperationException();
+	}
+
+}
